099fd6e30f1e595b773c6f17b83cc1a35034bc67
Cristian Perez <cristianperez@Cristians-MacBook-Pro.local>
2021-06-28 15:18:51 -0500

    carousel

--- client/components/App.js
+++ client/components/App.js
@@ -1,23 +1,21 @@
 import React, { Component } from "react";
 import Images from './Display Items/Images';
+import ImageCarousel from './Display Items/ImageCarousel'
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    // Initialize state.
-  
-  }
  
-
-  render() {
-    return <div className='App'>  
-              <Images/>
+    
+    
+    render() {
+      return <div className='App'>  
+              <Images images={this.props.images}/>
+              <ImageCarousel images={this.props.Carousel()}/>
           </div>
           
-       
-  }
-}
+          
+        }
+      }
+    
 
 export default App;
