ce2757f21a45ea42e26d7d0a48a9eab65246407b
ComplexChris <ChrisNguyen180@gmail.com>
2021-06-29 11:24:05 -0500

    Manual copy of integration branch

---
+++ client/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Images from "./Images/Images";
+import AddToCart from "./Charles/AddToCart";
+import Items from "./Items/Items";
+import SearchBar from "./Search Bar/SearchBar";
+
+class App extends Component {
+  componentDidMount() {
+    fetch("/api/cart")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <AddToCart />
+        <Items />
+        <Images images={this.props.images} />
+      </div>
+    );
+  }
+}
+
+export default App;
