a452571bd02edf9047d2fb84f2bc5f7841103775
ComplexChris <ChrisNguyen180@gmail.com>
2021-06-29 11:37:06 -0500

    New itegration structure.

---
+++ client/components/Charles/AddToCart.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import CurrentCart from './CurrentCart.js';
+import "./styles/styles.css";
+
+ class AddToCart extends React.Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            currentCart:false
+        }
+
+        this.clickEvent = this.clickEvent.bind(this);
+    }
+
+    clickEvent () {
+        this.setState({currentCart:!this.state.currentCart});
+      }
+
+    render () {
+        return (
+            <div>
+                
+                {this.state.currentCart && <CurrentCart 
+                currentCart={this.clickEvent}
+                />}
+
+                <div className={'ecommerceCard'}>
+                    <button className={'buyItNowBtn'}>Buy It Now</button>
+                    <button className={'addToCartBtn'} onClick={this.clickEvent}>Add to cart</button>
+                    <button className={'addToWatchlistBtn'}>Add to Watchlist</button>
+                    <div className={'heartImg'}> <img src='heart.svg' height='12px' width='12px'></img></div> 
+                </div>
+            </div>
+        );
+    }
+ }
+
+ export default AddToCart;
---
+++ client/components/Items/Items.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Component } from "react";
+
+class Items extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {},
+      displayItem: 4,
+    };
+  }
+  componentDidMount() {
+    fetch(`/api/items/${this.state.displayItem}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState(() => ({ item: data[0][0] }));
+      });
+  }
+  render() {
+    return (
+      <div className={"itemComponents"}>
+        <div className={"itemDescHeader"}>
+          <h1>{this.state.item.name}</h1>
+        </div>
+        <hr />
+        <div>
+          <div>
+            Condition: {this.state.item.description}
+            <br />
+          </div>
+          <div>
+            Model: awesome
+            <br />
+          </div>
+          <div>
+            <label htmlFor="quantity">Quantity:</label>
+            <input type="text" value="1" size="4" />
+            <br />
+          </div>
+          <hr />
+          <div>
+            Price:{" "}
+            <span className={"itemPrice"}>
+              {"US  " + this.state.item.price}
+            </span>
+            <br />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Items;
