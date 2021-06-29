70853a391de292ed9a410af71826f846e6f4de67
Cristian Perez <cristianperez@Cristians-MacBook-Pro.local>
2021-06-29 16:03:09 -0500

    added app.js to gitignore for troubleshooting purposes

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from "react";
-// import Images from "./Cristian/Images";
 import ImageCarousel from "./Images/ImageCarousel";
-// import Images from "./Images/Images";
-// import AddToCart from "./Charles/AddToCart";
-// import Items from "./Items/Items";
-// import SearchBar from "./Search Bar/SearchBar";
+import AddToCart from "./Charles/AddToCart";
+import Items from "./Items/Items";
+import SearchBar from "./Search Bar/SearchBar";
 
 class App extends Component {
   componentDidMount() {
@@ -16,9 +14,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        {/* <SearchBar />
+        <SearchBar />
         <AddToCart />
-        <Items /> */}
+        <Items />
         <ImageCarousel/>
        
       </div>
