1d042f12e0be2f220ae4e3a9722087ca70cbe93d
ComplexChris <ChrisNguyen180@gmail.com>
2021-06-30 14:17:55 -0500

    Fixed Search Bar component and Service; Corrected API routes/paths

--- client/components/App.js
+++ client/components/App.js
@@ -5,19 +5,38 @@ import Items from "./Items/Items";
 import SearchBar from "./Search Bar/SearchBar";
 
 class App extends Component {
+  constructor(){
+    super();
+    const user = window.localStorage.getItem("current_user")  || 1    // If no user is logged in, default to 1
+    this.state = {
+        current_item: 1,
+        user_id: Number.parseInt( user ),    // Can be null if logged out, create conditional
+
+    }
+  }
+
+  setUserID(new_id){
+    // Updates new user ID
+    this.setState( { user_id:new_id } )
+  }
+
+  setCurrentItem(item_id){
+    // Updates new item (if user clicks different item, for example)
+    this.setState( { current_item:item_id } )
+  }
+
+
   componentDidMount() {
-    fetch("/api/cart")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    // Deleted Cart fetch.
   }
 
   render() {
     return (
       <div>
-        <SearchBar />
-        <AddToCart />
-        <Items />
-        <ImageCarousel/>
+        <SearchBar change_user={ this.setUserID.bind( this ) } />
+        <AddToCart user_id={this.state.user_id} />
+        <Items current_item={this.state.current_item} />
+        <ImageCarousel current_item={this.state.current_item} />
       </div>
     );
   }
